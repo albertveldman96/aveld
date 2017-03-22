02212b581c97d15fe929b61c0e2a29eb94f905e9
Unknown <albertveldman96@gmail.com>
2017-03-22 18:19:08 +0100

    Added Plot component

---
+++ src/components/Plot.js
@@ -0,0 +1,35 @@
+/* global Plotly */
+import React, { Component } from 'react';
+
+class Plot extends Component {
+    drawPlot = () => {
+        Plotly.newPlot('plot', [{
+            x: this.props.xData,
+            y: this.props.yData,
+            type: this.props.type
+        }], {
+            margin: {
+                t: 0, r: 0, l:30
+            },
+            xaxis: {
+                gridcolor: 'transparant'
+            }
+        }, {
+            displayModeBar: false
+        });
+    };
+
+    componentDidMount() {
+        this.drawPlot();
+    }
+
+    componentDidUpdate() {
+        this.drawPlot();
+    }
+
+    render() {
+        return (<div id="plot"/>);
+    }
+}
+
+export default Plot;
