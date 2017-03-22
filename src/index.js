86f528685b742b43bf363e29b71d6fadbce4307f
Unknown <albertveldman96@gmail.com>
2017-03-22 19:06:57 +0100

    Fixed production bug

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ import App from './components/App';
 import reducer from './reducers';
 import './styles.css';
 
-let reduxDevToolConnector = undefined;
-if (process.env.NODE_ENV !== "production") {
-    if (window.__REDUX_DEVTOOLS_EXTENSION__) {
-        reduxDevToolConnector = window.__REDUX_DEVTOOLS_EXTENSION__();
-    } else {
-        console.log("REDUX DEVTOOLS: Not found, no connector installed on Redux Store.");
-    }
-}
+let store;
 const middleware = applyMiddleware(thunk);
-
-const store = createStore(
-    reducer,
-    compose(middleware, reduxDevToolConnector)
-);
+if (process.env.NODE_ENV !== "production" && window.__REDUX_DEVTOOLS_EXTENSION__) {
+    let reduxDevToolConnector = window.__REDUX_DEVTOOLS_EXTENSION__();
+    store = createStore(
+        reducer,
+        compose(middleware, reduxDevToolConnector)
+    );
+} else if (process.env.NODE_ENV !== "production") {
+    console.log("REDUX DEVTOOLS: Not found, no connector installed on Redux Store.");
+    store = createStore(reducer, middleware);
+} else {
+    store = createStore(reducer, middleware);
+}
 
 render(
     <Provider store={store}>
