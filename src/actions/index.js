02a8a8fdab754a7dae52281500afa32ae3d9483a
Unknown <albertveldman96@gmail.com>
2017-03-22 12:27:26 +0100

    Added redux-thunk dependency. Added redux structure.

---
+++ src/actions/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by albert on 22-3-17.
+ */
--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
+import reducer from './reducers';
 import './styles.css';
 
-ReactDOM.render(
-  <App />,
+let reduxDevToolConnector = undefined;
+if (process.env.NODE_ENV !== "production") {
+    if (window.__REDUX_DEVTOOLS_EXTENSION__) {
+        reduxDevToolConnector = window.__REDUX_DEVTOOLS_EXTENSION__();
+    } else {
+        console.log("REDUX DEVTOOLS: Not found, no connector installed on Redux Store.");
+    }
+}
+
+const store = createStore(reducer,applyMiddleware(thunk), reduxDevToolConnector);
+
+
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
   document.getElementById('root')
 );
