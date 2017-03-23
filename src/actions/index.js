2f0375708ee5afe03ee164bb1762cef28d265cbf
Unknown <albertveldman96@gmail.com>
2017-03-23 19:42:09 +0100

    Changed getWeather to use server-side API instead of direct request.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,33 +1,32 @@
-import xhr from 'xhr';
+import request from 'superagent';
 
-const urlPrefix = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-const urlSuffix = '&APPID=990bc587960e67fbd43cb5660a88ac3d&units=metric';
 export const getWeather = (city) => (
     dispatch =>
     {
         dispatch({type: 'GET_WEATHER_PENDING'});
         const cityEncoded = encodeURIComponent(city);
-        const url = urlPrefix + cityEncoded + urlSuffix;
-        xhr({
-            url: url
-        }, (err, data) => {
-            if (err)
-                dispatch({type: 'GET_WEATHER_FAILED', payload: err});
-            else {
-                const body = JSON.parse(data.body);
-                const list = body.list;
-                let dates = [];
-                let temps = [];
-                for (let i = 0; i < list.length; i++) {
-                    dates.push(list[i].dt_txt);
-                    temps.push(list[i].main.temp);
-                }
-                dispatch({
-                    type: 'GET_WEATHER_SUCCEEDED',
-                    payload: { dates, temps }
-                });
-            }
+        request
+            .get('/api/v1/weather/' + cityEncoded)
+            .set("Accept", 'application/json')
+            .end(function(err, data) {
+                if (err)
+                    dispatch({type: 'GET_WEATHER_FAILED', payload: err});
+                else {
+                    console.log(data.text);
+                    const body = JSON.parse(data.text);
 
-        })
+                    const list = body.list;
+                    let dates = [];
+                    let temps = [];
+                    for (let i = 0; i < list.length; i++) {
+                        dates.push(list[i].dt_txt);
+                        temps.push(list[i].main.temp);
+                    }
+                    dispatch({
+                        type: 'GET_WEATHER_SUCCEEDED',
+                        payload: { dates, temps }
+                    });
+                }
+            });
     }
 );
