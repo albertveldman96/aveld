5ba39a21608b6ed16e4b47cc68f19d77eba8ac69
Unknown <albertveldman96@gmail.com>
2017-03-23 19:40:22 +0100

    Moved secrets to server-side

---
+++ secrets_template.js
@@ -0,0 +1,7 @@
+const secrets = {
+    darksky_api: 'DARKSKY_API_KEY',
+    openweather_api: 'OPENWEATHER_API_KEY',
+    google_geocoding: 'GOOGLE_GEOCODING_API_KEY'
+};
+
+export default secrets;
--- src/secrets_template.js
+++
@@ -1,6 +0,0 @@
-const secrets = {
-    darksky_api: 'DARKSKY_API_KEY',
-    openweather_api: 'OPENWEATHER_API_KEY'
-};
-
-export default secrets;
