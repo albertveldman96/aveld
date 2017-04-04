887ec6951c263c74f8f17c2d5293c0ca4ce14649
Albert <albertveldman96@gmail.com>
2017-04-04 15:57:22 +0200

    Added getWeights actioncreator

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,11 +8,10 @@ export const getWeather = (city) => (
         request
             .get('/api/v1/weather/' + cityEncoded)
             .set("Accept", 'application/json')
-            .end(function(err, data) {
+            .end((err, data) => {
                 if (err)
                     dispatch({type: 'GET_WEATHER_FAILED', payload: err});
                 else {
-                    console.log(data.text);
                     const body = JSON.parse(data.text);
 
                     const list = body.list;
@@ -26,6 +25,35 @@ export const getWeather = (city) => (
                         type: 'GET_WEATHER_SUCCEEDED',
                         payload: { dates, temps }
                     });
+                }
+            });
+    }
+);
+
+export const getWeights = () => (
+    dispatch => {
+        dispatch({type: 'GET_WEIGHTS_PENDING'});
+        request
+            .get('/api/v1/weights')
+            .set('Accept', 'application/json')
+            .end((err, data) => {
+                if (err)
+                    dispatch({type: 'GET_WEIGHTS_FAILED', payload: err});
+                else {
+                    const body = JSON.parse(data.text);
+                    console.log(data.text);
+
+                    let weights = [];
+                    let dates = [];
+                    for (let i = 0; i < body.length; i++) {
+                        weights.push(body[i].weight);
+                        dates.push(body[i].date.day);
+                    }
+
+                    dispatch({
+                        type: 'GET_WEIGHTS_SUCCEEDED',
+                        payload: { weights, dates }
+                    })
                 }
             });
     }
