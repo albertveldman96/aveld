538f2f23aed7bdf7c8cd16ad215564cd2059e1ab
Albert <albertveldman96@gmail.com>
2017-04-04 15:53:31 +0200

    added mongodb authentication

--- secrets_template.js
+++ secrets_template.js
@@ -1,7 +1,14 @@
 const secrets = {
-    darksky_api: 'DARKSKY_API_KEY',
-    openweather_api: 'OPENWEATHER_API_KEY',
-    google_geocoding: 'GOOGLE_GEOCODING_API_KEY'
+    apis: {
+        darksky_api: '',
+        openweather_api: '',
+        google_geocoding: ''
+    },
+    mongodb: {
+        url: '',
+        username: '',
+        password: ''
+    }
 };
 
-export default secrets;
+module.exports.secrets = secrets;
